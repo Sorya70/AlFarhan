bf2d4db7b8df976b993ef0326400840e304be54e
Sorya Rathore <surjeetkhalasi.com>
2025-07-12 14:52:57 +0530

    hero update

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -84,20 +84,14 @@ const HeroSection = ({ dict }) => {
                     key={slide.id}
                     className="relative h-[400px] sm:h-[350px] md:h-[450px] rounded-lg overflow-hidden"
                   >
-                    {/* Blurred Red Overlay */}
-                    <div className="absolute inset-0 z-0">
-                      <Image
-                        src={`${slide.image}`}
-                        alt={slide.name}
-                        fill
-                        className="object-cover filter blur-xs brightness-100"
-                        priority
-                      />
-                      <div className="absolute inset-0 bg-red-600/20 "></div>
-                    </div>
-
-                    {/* Slide content */}
-                    <div className="absolute inset-0 z-10 flex items-center">
+                    <Image
+                      src={`${slide.image}`}
+                      alt={slide.name}
+                      fill
+                      className="object-cover"
+                      priority
+                    />
+                    <div className="absolute inset-0 bg-black/40 flex items-center">
                       <div className="text-white p-4 md:p-6 max-w-xl">
                         <span className="inline-block bg-sky-600 text-white px-4 py-1 rounded-full text-xs md:text-sm font-medium mb-2 md:mb-3">
                           {slide.discount}
@@ -110,8 +104,13 @@ const HeroSection = ({ dict }) => {
                         </p>
                         <ul className="mb-3 md:mb-4 space-y-0 md:space-y-1">
                           {slide.features.map((feature, index) => (
-                            <li key={index} className="flex items-center text-sm">
-                              <span className="mx-2 text-lg text-sky-500 font-bold">✓</span>
+                            <li
+                              key={index}
+                              className="flex items-center text-sm"
+                            >
+                              <span className="mx-2 text-lg text-sky-500 font-bold">
+                                ✓
+                              </span>
                               {feature}
                             </li>
                           ))}
@@ -127,7 +126,6 @@ const HeroSection = ({ dict }) => {
                       </div>
                     </div>
                   </SwiperSlide>
-
                 );
               })}
             </Swiper>
